a088dd20c509fa30fb7c6274ddba9ff44f011726
Randall19 <50095691+Randall19@users.noreply.github.com>
2025-02-10 10:13:46 -0700

    Update cis129_lab02_shortStory.py
    A short story that warns of the danger of a potential global pandemic.

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,9 @@
-print('Hello World')
+# Author:  Randall Brown
+# Course:  CIS129 Introduction to Python 
+# This is a short story to learn how to use Github
+# Story Title:  Are we due for another pandemic?
+print('In early 2020 the world was engulfed in a Covid-19 pandemic.')
+print('Viruses endemic in animals can evolve to infect humans.')
+print('Currently the H5N1 and H7N1 influenza virus is infecting birds and cows')
+print('If this pathologic avian influenza virus learns to infect and transmit easily between human,')
+print('then a new global pandemic could start to infect humans around the world.")
